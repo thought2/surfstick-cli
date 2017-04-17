d7b2dfbd02d9cd9e7eb4d80080dfcce6868b330c
Thought2 <me@thought2.de>
2017-04-17 18:21:42 +0200

    nix

--- nix-deps.nix
+++ nix-deps.nix
@@ -1,3 +1,24 @@
 { self, fetchurl, fetchgit ? null, lib }:
 
-{}
+{
+  by-spec."lodash"."^4.17.4" =
+    self.by-version."lodash"."4.17.4";
+  by-version."lodash"."4.17.4" = self.buildNodePackage {
+    name = "lodash-4.17.4";
+    version = "4.17.4";
+    bin = false;
+    src = fetchurl {
+      url = "https://registry.npmjs.org/lodash/-/lodash-4.17.4.tgz";
+      name = "lodash-4.17.4.tgz";
+      sha1 = "78203a4d1c328ae1d86dca6460e369b57f4055ae";
+    };
+    deps = {
+    };
+    optionalDependencies = {
+    };
+    peerDependencies = [];
+    os = [ ];
+    cpu = [ ];
+  };
+  "lodash" = self.by-version."lodash"."4.17.4";
+}
